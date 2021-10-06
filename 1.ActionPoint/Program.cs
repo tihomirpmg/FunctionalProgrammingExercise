09f32ec7ecbded5efa70b5ed08f49baafb652898
Тихомир Найденов <tihomirnaydenov02@pmgkk.com>
2021-10-06 08:38:11 +0300

    Add project files.

---
+++ 1.ActionPoint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1.ActionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Action<string[]> print = PrintNames;
+            print(names);
+        }
+
+        static void PrintNames(string[] items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 1.ActionPoint/README.md
@@ -0,0 +1,3 @@
+﻿#Collection of Strings
+
+##This app reads a collection of strings from the console and then prints them onto the console each word on new line.
